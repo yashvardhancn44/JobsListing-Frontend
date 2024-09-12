1975eb06869aa394c4121dc2e6b103161ed90136
yashvardhancn44 <zbti.dev@gmail.com>
2024-09-12 17:25:13 +0530

    using useParam to get id and using useEffect for JobPage

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,5 +1,27 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
 const JobPage = () => {
-  return <div>JobPage</div>;
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log("Error Fecthing data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, []);
+
+  return loading ? <Spinner /> : <h1>{job.title}</h1>;
 };
 
 export default JobPage;
