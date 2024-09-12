443dd5f75e8235783def89d343d44d15d33803f7
yashvardhancn44 <zbti.dev@gmail.com>
2024-09-12 17:16:08 +0530

    Proxying and Creating JobPage routing

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobPage from "./pages/JobPage";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -17,6 +18,7 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="*" element={<NotFoundPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} />
       </Route>
     )
   );
