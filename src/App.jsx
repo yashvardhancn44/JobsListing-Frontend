f843304480b95cb862b7d7ebd32a99f76e23f832
yashvardhancn44 <zbti.dev@gmail.com>
2024-09-12 12:33:41 +0530

    Adding Navbar and Hero Component and working with Props, default values of props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,12 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
     <>
-      <nav className="bg-indigo-700 border-b border-indigo-500">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="flex h-20 items-center justify-between">
-            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-              {/* <!-- Logo --> */}
-              <a
-                className="flex flex-shrink-0 items-center mr-4"
-                href="/index.html"
-              >
-                <img
-                  className="h-10 w-auto"
-                  src="images/logo.png"
-                  alt="React Jobs"
-                />
-                <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                  React Jobs
-                </span>
-              </a>
-              <div className="md:ml-auto">
-                <div className="flex space-x-2">
-                  <a
-                    href="/index.html"
-                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
-                    Home
-                  </a>
-                  <a
-                    href="/jobs.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
-                    Jobs
-                  </a>
-                  <a
-                    href="/add-job.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
-                    Add Job
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      {/* <!-- Hero --> */}
-      <section className="bg-indigo-700 py-20 mb-4">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
-          <div className="text-center">
-            <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-              Become a React Dev
-            </h1>
-            <p className="my-4 text-xl text-white">
-              Find the React job that fits your skills and needs
-            </p>
-          </div>
-        </div>
-      </section>
-
+      <Navbar />
+      {/* <Hero title="Test title" subtitle="This is the subtitle" /> */}
+      <Hero />
       {/* <!-- Developers and Employers --> */}
       <section className="py-4">
         <div className="container-xl lg:container m-auto">
