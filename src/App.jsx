f26a87af885fb24185e76bd3b3dc5351d21f83e9
yashvardhancn44 <zbti.dev@gmail.com>
2024-09-13 09:22:59 +0530

    UpdateJobPage, Update Form, PUT request to update Job

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 //Addjob
 const App = () => {
@@ -34,6 +35,18 @@ const App = () => {
     return;
   };
 
+  //Update Job
+  const updateJob = async (updatedJob) => {
+    const res = await fetch(`/api/jobs/${updateJob.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateJob),
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
@@ -43,6 +56,11 @@ const App = () => {
         <Route
           path="/jobs/:id"
           element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
           loader={jobLoader}
         />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
