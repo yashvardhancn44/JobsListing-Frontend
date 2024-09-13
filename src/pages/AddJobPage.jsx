0669a9a4078736c0173c8e1b5031c4836cbc8ac9
yashvardhancn44 <zbti.dev@gmail.com>
2024-09-13 09:03:00 +0530

    Added React-toastify

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AddJobPage = ({ addJobSubmit }) => {
   const [title, setTitle] = useState("");
@@ -31,6 +32,7 @@ const AddJobPage = ({ addJobSubmit }) => {
       },
     };
     addJobSubmit(newJob);
+    toast.success("Job added successfully");
     return navigate("/jobs");
   };
 
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,6 +1,7 @@
 import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const JobPage = ({ deleteJob }) => {
   const navigate = useNavigate();
@@ -13,6 +14,8 @@ const JobPage = ({ deleteJob }) => {
     );
     if (!confirm) return;
     deleteJob(jobId);
+    toast.success("Job deleted successfully");
+
     navigate("/jobs");
   };
 
